75743bb6448d8251375f949773d23c2165a2bb13
yukiko-kato-bass <springcomesagain129@gamil.com>
2013-12-18 19:00:07 +0900

    Facadeパターン追加

---
+++ src/facade/pagemaker/Main.java
@@ -0,0 +1,12 @@
+package facade.pagemaker;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PageMaker.makeWelcomePage("ykato@ykato.com", "welcome.html");
+	}
+
+}
