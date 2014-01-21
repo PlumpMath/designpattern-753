d620bdb509c438de81064ef541c52213a3eed5c6
yukiko-kato-bass <springcomesagain129@gamil.com>
2014-01-21 15:32:17 +0900

    Cain of Responsibilityパターン追加

---
+++ src/chainofresponsibility/Trouble.java
@@ -0,0 +1,16 @@
+package chainofresponsibility;
+
+public class Trouble {
+
+	private int number;
+	public Trouble(int number) {
+		this.number = number;
+	}
+	public int getNumber() {
+		return this.number;
+	}
+	@Override
+	public String toString() {
+		return "[Trouble " + this.number + "]";
+	}
+}
